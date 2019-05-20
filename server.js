190fb2babbe2225962aaa3561963687f9689e9bd
Riley Worthington <worthirj@whitman.edu>
2019-05-20 14:48:38 -0700

    change endpoints

--- server.js
+++ server.js
@@ -9,10 +9,16 @@ app.use(bodyParser.json());
 
 app.get('/login', authorizeStrava);
 
-app.get('/', (req, res) => {
+app.get('/subscription', (req, res) => {
   const challenge = req.query['hub.challenge'];
   res.setHeader('Content-Type', 'application/json');
   res.status(200).send(JSON.stringify({ 'hub.challenge': challenge }));
+})
+
+app.post('/subscription', (req, res) => {
+  res.sendStatus(200);
+  const { object_type, aspect_type, object_id } = req.body;
+  console.log(object_type, aspect_type, object_id);
 })
 
 app.post('/', (req, res) => {
@@ -23,5 +29,5 @@ app.post('/', (req, res) => {
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
-  console.log('We are live on ' + port);
+  console.log('Listening on port ' + port);
 });

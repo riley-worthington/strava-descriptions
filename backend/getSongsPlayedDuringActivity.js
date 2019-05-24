722338120f652f40793d3fc673645bd6bc1925e8
Riley Worthington <worthirj@whitman.edu>
2019-05-24 10:40:19 -0700

    move backend files into backend folder

--- backend/getSongsPlayedDuringActivity.js
+++ backend/getSongsPlayedDuringActivity.js
@@ -3,7 +3,7 @@ const getSongsPlayedDuringActivity = (songHistory, startTime, endTime) => {
   // startTime and endTime are Unix timestamps in milliseconds
   const filterByTime = song => {
     const playedAt = Date.parse(song.played_at);
-    return playedAt >= startTime && playedAt <= endTime;
+    return (playedAt >= startTime) && (playedAt <= endTime);
   };
 
   const duringActivity = songHistory.filter(filterByTime);
